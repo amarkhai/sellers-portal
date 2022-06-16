b4e4a87fd57e970f497d33da45485da7a4948cca
Andrew Markhai <buk2018irinam@gmail.com>
2022-06-16 23:41:46 +0300

    Подключил bulma и сделал тестовый шаблон

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,8 @@
 import {createApp} from 'vue';
+import App from './js/App.vue';
+import bulma from 'bulma';
 
-const app = createApp({
-    /* root component options */
-})
+const app = createApp(App);
+app.mount('#app');
 
 console.log('Boring JavaScript file: make me cooler!');
