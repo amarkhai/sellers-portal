dfe98897af26e8d634369121bd53217888066adb
Andrew Markhai <buk2018irinam@gmail.com>
2022-06-29 19:30:46 +0300

    Установил доктрину

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
